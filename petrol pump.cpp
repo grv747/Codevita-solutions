e2c405fa06974c51198a10cb4450a8e1e8890b6d
grv747 <65960407+grv747@users.noreply.github.com>
2020-07-31 22:16:58 +0530

    Add files via upload

--- petrol pump.cpp
+++ petrol pump.cpp
@@ -1,64 +1,59 @@
-#include <iostream>
+#include<iostream>
+#include<string>
 #include<sstream>
 #include<vector>
-#include<cmath>
 using namespace std;
-
 int main()
 {
-
-	    int sum=0,i,n,x,j,t;
-	    string s;
-     vector<int> v1;
-      getline(cin, s, '\n');
-      stringstream ss(s);
-     while(ss>>t)
+    int n;
+    string str;
+    getline(cin,str);
+    stringstream ss(str);
+    vector<int>v;
+    while(ss>>n)
     {
-        sum+=t;
-        v1.push_back(t);
+        v.push_back(n);
     }
-    n=v1.size();
+   int rows=v.size();
+   int sum=0,i,j,x;
+   for(i=0;i<v.size();i++)
+   {
+       sum=sum+v[i];
+   }
+    bool dp[rows+1][sum+1];
+   for(i=1;i<sum+1;i++)
+   {
+      dp[0][i]=false;
+   }
+   for(i=0;i<rows+1;i++)
+   {
+       dp[i][0]=true;
+   }
+   for(i=1;i<rows+1;i++)
+   {
+       for(j=1;j<sum+1;j++)
+       {
+           if(j<v[i-1])
+           {
+               dp[i][j]=dp[i-1][j];
+           }
+           else
+           {
+               dp[i][j]=dp[i-1][j]||dp[i-1][j-v[i-1]];
+           }
 
-	    int a[n];
-	    for(i=0;i<n;i++)
-	    {
-	       a[i]=v1[i];
-	    }
+       }
+   }
+   for(j=sum/2;j>=0;j--)
+   {
+       if(dp[rows][j]==true)
+       {
+            x=j;
+           break;
+       }
+   }
 
-	    bool dp[n+1][sum+1];
-	    for(i=1;i<sum+1;i++)
-	    {
-	        dp[0][i]=false;
-	    }
-	    for(i=0;i<n+1;i++)
-	    {
-	        dp[i][0]=true;
-	    }
-	    for(i=1;i<n+1;i++)
-	    {
-	        for(j=1;j<sum+1;j++)
-	        {
-	            if(j>=a[i-1])
-	            {
-	                dp[i][j]=dp[i-1][j]||dp[i-1][j-a[i-1]];
-	            }
-	            else
-	            {
-	                dp[i][j]=dp[i-1][j];
-	            }
-	        }
-	    }
-	    for(j=sum/2;j>=0;j--)
-	    {
-	        if(dp[n][j]==true)
-	        {
-	           x=sum-2*j;
-	            break;
-	        }
-	    }
-	    x=x+j;
 
-	   printf("%d\n",x);
+   printf("%d",max(x,sum-x));
 
-	return 0;
 }
